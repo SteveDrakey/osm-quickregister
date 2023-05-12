4ae480f73f309cb143c5f1dbd69f6e3bf2337a34
Steve Drake <sdrake@truenorthit.co.uk>
2023-05-12 15:03:32 +0100

    auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'osm-quickregister';
+
+  constructor(public auth: AuthService, private client: HttpClient ) {}
+  
+  async test() {
+    // make http call
+    var result = await this.client.get('https://www.onlinescoutmanager.co.uk/ext/members/attendance/?action=get&sectionid=35289&termid=626418&section=scouts&nototal=true').toPromise();
+    
+    console.log(result);
+
+    this.auth.idTokenClaims$.subscribe((claims) => console.log(claims));
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => console.log(isAuthenticated));
+    this.auth.user$.subscribe((profile) => console.log(profile));
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,64 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthHttpInterceptor } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      // The domain and clientId were configured in the previous chapter
+      domain: '8-26-scouts-todmorden.uk.auth0.com',
+      clientId: 'SCLeT8ZIxCihEMKJeehT0KrJcNChRsUr',
+    
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+        
+        // Request this audience at user authentication time
+        audience: 'https://8-26-scouts-todmorden.uk.auth0.com/api/v2/',
+    
+        // Request this scope at user authentication time
+        scope: 'read:current_user',
+      },
+    
+      // Specify configuration for the interceptor              
+      httpInterceptor: {
+        allowedList: [
+          {
+            // Match any request that starts 'https://8-26-scouts-todmorden.uk.auth0.com/api/v2/' (note the asterisk)
+            uri: 'https://www.onlinescoutmanager.co.uk/ext/members/*',
+            // // tokenOptions: {
+            // //   authorizationParams: {
+            // //     // The attached token should target this audience
+            // //     audience: 'https://8-26-scouts-todmorden.uk.auth0.com/api/v2/',
+    
+            // //     // The attached token should have these scopes
+            // //     scope: 'read:current_user'
+            // //   }
+            // }
+          }
+        ]
+      }
+    })
+    // AuthModule.forRoot({
+    //   domain: '8-26-scouts-todmorden.uk.auth0.com',
+    //   clientId: 'SCLeT8ZIxCihEMKJeehT0KrJcNChRsUr',
+    //   authorizationParams: {
+    //     redirect_uri: window.location.origin
+    //   }
+    // }),
+
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
