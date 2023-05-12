1c4d548abc214aa2290bf672c33a0a32c069cb3d
Steve Drake <sdrake@truenorthit.co.uk>
2023-05-12 16:11:18 +0100

    fetch

--- netlify/functions/patrols/patrols.ts
+++ netlify/functions/patrols/patrols.ts
@@ -1,10 +1,11 @@
 import { Handler } from '@netlify/functions'
+import fetch from 'node-fetch'
 
 export const handler: Handler = async (event, context) => {
   const url = `https://www.onlinescoutmanager.co.uk/ext/members/patrols/?action=getPatrolsWithPeople&sectionid=35289&termid=626418&include_no_patrol=y`;
   const response = await fetch(url, {
     headers :{
-      'Authorization': event.headers['authorization'] || '',
+      'authorization': event.headers['authorization'] || '',
     }
   });
   const data = await response.json();
